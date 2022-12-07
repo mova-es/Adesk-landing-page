560863096457e3d3f6c413c93929996d4f290912
Elvira <mova.es94@gmail.com>
2022-12-08 00:42:53 +0500

    initial commit

---
+++ code.js
@@ -0,0 +1,41 @@
+const question1 = document.querySelector('.question1');
+const hide1 = document.querySelector('.hide1');
+
+question1.addEventListener('click', () => {
+	hide1.classList.toggle('hide1');
+});
+
+const question2 = document.querySelector('.question2');
+const hide2 = document.querySelector('.hide2');
+
+question2.addEventListener('click', () => {
+	hide2.classList.toggle('hide2');
+});
+
+const question3 = document.querySelector('.question3');
+const hide3 = document.querySelector('.hide3');
+
+question3.addEventListener('click', () => {
+	hide3.classList.toggle('hide3');
+});
+
+const question4 = document.querySelector('.question4');
+const hide4 = document.querySelector('.hide4');
+
+question4.addEventListener('click', () => {
+	hide4.classList.toggle('hide4');
+});
+
+const question5 = document.querySelector('.question5');
+const hide5 = document.querySelector('.hide5');
+
+question5.addEventListener('click', () => {
+	hide5.classList.toggle('hide5');
+});
+
+const question6 = document.querySelector('.question6');
+const hide6 = document.querySelector('.hide6');
+
+question6.addEventListener('click', () => {
+	hide6.classList.toggle('hide6');
+});
